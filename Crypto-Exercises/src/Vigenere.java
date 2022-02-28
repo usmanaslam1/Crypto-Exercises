1fbde46b574a154c29e4dee38fe38284cce7912f
Usman <usman@usman.uk>
2022-02-28 14:28:14 -0800

    Added Vigenere

---
+++ Crypto-Exercises/src/Vigenere.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+import com.usman.ciphers.VigenereCipher;
+
+public class Vigenere {
+
+        public static void main (String args[]) throws Exception {
+
+
+                String a="HELLO";
+                String b="WORLD";
+
+
+                /*System.out.println("Enter plaintext : ");
+                System.out.println("Enter key : ");
+                Scanner scan=new Scanner(System.in);*/
+
+
+                //String pt=scan.next();
+                //String k=scan.next();
+
+
+
+                /*String s="HELLOWORLD";
+                String k="ISO";
+                String e=VigenereCipher.encrypt(s,k);
+                String d=VigenereCipher.decrypt(e,k);
+
+                System.out.println("Plain Text: "+s);
+                System.out.println("Key: "+VigenereCipher.getKey());
+                System.out.println("Cipher Text: "+e);*/
+        }
+
+}
