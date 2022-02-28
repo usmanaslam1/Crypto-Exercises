0a20135d33d8ccb9b5a15ac145ee3f2a34f39f4a
Usman <usman@usman.uk>
2022-02-28 14:46:16 -0800

    Added test case

---
+++ Crypto-Exercises/src/com/usman/ciphers/VigenereCipherTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.usman.ciphers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author usman
+ *
+ */
+class VigenereCipherTest extends VigenereCipher {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.usman.ciphers.VigenereCipher#getKey()}.
+	 */
+	@Test
+	void testGetKey() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.usman.ciphers.VigenereCipher#encrypt(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testEncrypt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.usman.ciphers.VigenereCipher#decrypt(java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testDecrypt() {
+		fail("Not yet implemented");
+	}
+
+}
